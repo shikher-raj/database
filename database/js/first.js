4a420f33fb8d62c4986d2e46b86b6458a1e4f1b0
Shikher Raj <32222926+shikher-raj@users.noreply.github.com>
2018-12-24 20:32:49 +0530

    Create first.js

---
+++ database/js/first.js
@@ -0,0 +1,25 @@
+var a="user";
+function f1(){
+	document.getElementById("l1").innerHTML = "add a new " + a;
+}
+function f2(){
+	document.getElementById("l2").innerHTML = "find a " + a;
+}
+function f3(){
+	document.getElementById("l3").innerHTML = "update a " + a;
+}
+function f4(){
+	document.getElementById("l4").innerHTML = "delete a " + a;
+}
+function fe1(){
+	document.getElementById("l1").innerHTML = "";
+}
+function fe2(){
+	document.getElementById("l2").innerHTML = "";
+}
+function fe3(){
+	document.getElementById("l3").innerHTML = "";
+}
+function fe4(){
+	document.getElementById("l4").innerHTML = "";
+}
